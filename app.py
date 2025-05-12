01673bbc72e78329cf38d576a1dc551e5c08c2a8
rialunochka <valeriiakvasha@gmail.com>
2025-05-11 20:06:58 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, send_from_directory
+import pandas as pd
+import os
+
+app = Flask(__name__)
+CSV_FILE = "clips_database.csv"
+CLIPS_FOLDER = "static/clips"
+
+@app.route('/')
+def index():
+    if not os.path.exists(CSV_FILE):
+        return "clips_database.csv not found"
+    df = pd.read_csv(CSV_FILE)
+    return render_template("index.html", clips=df.to_dict(orient="records"))
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(CLIPS_FOLDER, filename, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
