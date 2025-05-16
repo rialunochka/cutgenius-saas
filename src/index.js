151453179594141001e07ec7039b2ae11cbe2cfd
rialunochka <valeriiakvasha@gmail.com>
2025-05-16 16:39:42 -0700

    index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';      // your Tailwind entry
+import CutGeniusApp from './CutGeniusApp';
+
+ReactDOM.render(<CutGeniusApp />, document.getElementById('root'));
